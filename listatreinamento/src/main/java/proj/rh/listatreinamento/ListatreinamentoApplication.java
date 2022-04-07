5bec2827b80fc8c72f26a067d6031450dc2da23c
mzsmatheus <63678546+mzsmatheus@users.noreply.github.com>
2022-04-07 18:22:39 -0300

    primeiro commit

---
+++ listatreinamento/src/main/java/proj/rh/listatreinamento/ListatreinamentoApplication.java
@@ -0,0 +1,13 @@
+package proj.rh.listatreinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListatreinamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListatreinamentoApplication.class, args);
+	}
+
+}

731687becdbd23e2473446bae43e68ef0c2ed0e5
mzsmatheus <63678546+mzsmatheus@users.noreply.github.com>
2022-04-07 20:32:40 -0300

    CRUD com setor

---
+++ listatreinamento/src/main/java/proj/rh/listatreinamento/repository/PessoaRepository.java
@@ -0,0 +1,12 @@
+package proj.rh.listatreinamento.repository;
+import org.springframework.data.repository.CrudRepository;
+
+import proj.rh.listatreinamento.domain.Pessoa;
+
+
+
+public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
+    
+}
+
+
